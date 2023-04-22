7d23b9e417227978fbe6d2f6006b01f964339397
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-04-22 10:40:39 +0300

    Done

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Item from "./Item";
 
-function Filter({ onCategoryChange }) {
+
+function Filter({ onCategoryChange, items, onSearchChange }) {
+  const [searchText, setSearchText] = useState('');
+
+  const filteredItems = items.filter((item)=>item.name.toLowerCase().includes(searchText.toLowerCase())
+  );
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" value={searchText} onChange={(e)=>{
+        setSearchText(e.target.value);
+        onSearchChange(e.target.value);
+      }} placeholder="Search Item....."/>
+      <ul>
+        {filteredItems.map((item)=>(
+          <Item item={item} />
+        ))}
+      </ul>
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
